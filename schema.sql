6bd8470c06a123bcc7c417a40b46ee77dade6757
Ronald Pitts <ronpitts81@gmail.com>
2020-09-08 17:56:12 -0400

    new scheme

--- schema.sql
+++ schema.sql
@@ -1,28 +1,26 @@
-DROP DATABASE IF EXISTS employee;
+DROP DATABASE IF EXISTS employees;CREATE DATABASE employees;
 
-CREATE DATABASE employee;
+USE employees;
 
-USE employee;
+CREATE TABLE department 
+(  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+ name VARCHAR(30) UNIQUE NOT NULL);
 
+CREATE TABLE role
+ (  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,  
+ title VARCHAR(30) UNIQUE NOT NULL, 
+ salary DECIMAL UNSIGNED NOT NULL, 
+ department_id INT UNSIGNED NOT NULL, 
+ INDEX dep_ind (department_id),
+ CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE);
 
-CREATE TABLE department (
-  head varchar(30), 
-  PRIMARY KEY (id),
-);
-
-CREATE TABLE  roles (
-     PRIMARY KEY (id)
-    title varchar(30) NOT NULL,
-    id int NOT NULL AUTO_INCREMENT,
-    department_id int NOT NULL AUTO_INCREMENT,
-   
-);
-
-CREATE TABLE employee (
-    PRIMARY KEY (id)
-    first_name varchar(30) NOT NULL,
-    last_name varchar(30) NOT NULL,
-    role_id int NOT NULL AUTO_INCREMENT,
-    manager_id int NOT NULL AUTO_INCREMENT,
-);
-
+CREATE TABLE employee 
+(  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+ first_name VARCHAR(30) NOT NULL,  
+ last_name VARCHAR(30) NOT NULL, 
+ role_id INT UNSIGNED NOT NULL,
+ INDEX role_ind (role_id), 
+ CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE, 
+ manager_id INT UNSIGNED, 
+ INDEX man_ind (manager_id), 
+ CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL);
